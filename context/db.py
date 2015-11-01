eb772598bcaa3ce12bed6eccdc98ae6144d96154
Ian Miell <ian.miell@gmail.com>
2015-11-01 18:46:55 +0000

    db.py

---
+++ context/db.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import psycopg2
+import psycopg2.extras
+ 
+def main():
+	conn_string = "host='localhost' dbname='alert_on_change' user='postgres' password='password'"
+	# get a connection, if a connect cannot be made an exception will be raised here
+	conn = psycopg2.connect(conn_string)
+	# HERE IS THE IMPORTANT PART, by specifying a name for the cursor
+	# psycopg2 creates a server-side cursor, which prevents all of the
+	# records from being downloaded at once from the server.
+	cursor = conn.cursor('cursor_unique_name', cursor_factory=psycopg2.extras.DictCursor)
+	# execute our Query
+	cursor.execute("select alert_on_change_id, command, output, common_threshold, email_address from alert_on_change")
+ 
+	# Because cursor objects are iterable we can just call 'for - in' on
+	# the cursor object and the cursor will automatically advance itself
+	# each iteration.
+	# This loop should run 1000 times, assuming there are at least 1000
+	# records in 'my_table'
+	row_count = 0
+	for row in cursor:
+		row_count += 1
+		print "row: %s    %s\n" % (row_count, row)
+	# retrieve the records from the database
+	records = cursor.fetchall()
+ 
+ 
+if __name__ == "__main__":
+	main()
+
