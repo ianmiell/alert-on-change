349d63426e64c929a06d10b87ad10c96c260e0e0
Ian Miell <ian.miell@gmail.com>
2015-11-01 11:55:40 +0000

    run

--- context/run.sh
+++ context/run.sh
@@ -6,26 +6,32 @@ rm -rf /tmp/mail
 mkdir -p /tmp/mail
 for item in $(cat /tmp/alert_on_change.csv)
 do
-    ID=$(cut -d! -f1 <(echo $item))
-    COMMAND=$(cut -d! -f2 <(echo $item))
+	ID=$(cut -d! -f1 <(echo $item))
+	COMMAND=$(cut -d! -f2 <(echo $item))
 	# Take the old output, and replace the \n with spaces as per our comparison
-    OLD_OUTPUT=$(cut -d! -f3 <(echo $item) | sed 's/\\n/ /g')
-    COMMON_THRESHOLD=$(cut -d! -f4 <(echo $item))
-    EMAIL_ADDRESS=$(cut -d! -f5 <(echo $item))
-    NEW_OUTPUT=$(eval $COMMAND)
+	OLD_OUTPUT=$(cut -d! -f3 <(echo $item) | sed 's/\\n/ /g')
+	COMMON_THRESHOLD=$(cut -d! -f4 <(echo $item))
+	EMAIL_ADDRESS=$(cut -d! -f5 <(echo $item))
+	NEW_OUTPUT=$(eval $COMMAND)
 	# Diff old and new output, and extract the common words percentage
-    COMMON=$(dwdiff -s <(echo $OLD_OUTPUT) <(echo $NEW_OUTPUT) 2>&1 > /dev/null | tail -1 | sed 's/.* \([0-9]\+\)..common.*/\1/')
-    #echo $OLD_OUTPUT
-    #echo $NEW_OUTPUT
-    #echo $(dwdiff -s <(echo $OLD_OUTPUT) <(echo $NEW_OUTPUT))
-    #echo $COMMON
-    #echo $COMMON_THRESHOLD
+	COMMON=$(dwdiff -s <(echo $OLD_OUTPUT) <(echo $NEW_OUTPUT) 2>&1 > /dev/null | tail -1 | sed 's/.* \([0-9]\+\)..common.*/\1/')
+	echo ================================================================================
+	echo $OLD_OUTPUT
+	echo ================================================================================
+	echo $NEW_OUTPUT
+	echo ================================================================================
+	echo $(dwdiff -s <(echo $OLD_OUTPUT) <(echo $NEW_OUTPUT))
+	echo ================================================================================
+	echo $COMMON
+	echo ================================================================================
+	echo $COMMON_THRESHOLD
+	echo ================================================================================
 	# If the common words percentage is less than the threshold, trigger an alert
-    if [[ $COMMON -lt $COMMON_THRESHOLD ]]
-    then
-        echo "update alert_on_change set output = '$NEW_OUTPUT', last_updated=now()  where alert_on_change_id = $ID" | psql alert_on_change
-        echo "Output of '$COMMAND' has less than $COMMON_THRESHOLD per cent in common with previous" >> /tmp/mail/${EMAIL_ADDRESS}
+	if [[ $COMMON -lt $COMMON_THRESHOLD ]]
+	then
+		echo "update alert_on_change set output = '$NEW_OUTPUT', last_updated=now()  where alert_on_change_id = $ID" | psql alert_on_change
+		echo "Output of '$COMMAND' has less than $COMMON_THRESHOLD per cent in common with previous" >> /tmp/mail/${EMAIL_ADDRESS}
 	else
-        echo "update alert_on_change set last_updated=now()  where alert_on_change_id = $ID" | psql alert_on_change
-    fi
+		echo "update alert_on_change set last_updated=now() where alert_on_change_id = $ID" | psql alert_on_change
+	fi
 done
