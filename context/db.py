350b98c74ba8f061cfc4481ed2688f72f1d8bac0
Ian Miell <ian.miell@gmail.com>
2015-11-16 14:23:04 +0000

    echo

--- context/db.py
+++ context/db.py
@@ -46,7 +46,7 @@ def main():
 		cursor2 = conn.cursor()
 		if common_percent < int(common_threshold):
 			cursor2.execute("""update alert_on_change set output=%s, last_updated=now() where alert_on_change_id = %s""",(new_output.encode('latin_1'),alert_on_change_id))
-			commands.getoutput('''echo "Output of command described as "''' + description + '''" has changed: ''' + command + '''" | mail -s "alert" --debug-level=100 ''' + email_address)
+			commands.getoutput('''echo Output of command described as: ''' + description + ''' has changed. | mail -s "alert" --debug-level=100 ''' + email_address)
 		commands.getoutput('rm -f /tmp/new /tmp/old')
 	conn.commit()
 
