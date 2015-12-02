2a479b7335032a6e002eec98148242fc6b319f1d
ianmiell <ian.miell@gmail.com>
2015-12-02 08:50:58 +0000

    insert template.sh

---
+++ context/insert_template.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+echo 'input command'
+read command
+echo 'email_address'
+read email_address
+echo 'description'
+read description
+echo 'output (empty is ok)'
+read output
+echo 'cadence (empty is ok)'
+read cadence
+echo 'common_threshold (empty is ok)'
+read common_threshold
+echo 'ignore_output (empty is ok)'
+read ignore_output
+
+if [[ $common_threshold = '' ]]
+then
+	common_threshold=100
+fi
+if [[ $cadence = '' ]]
+then
+	cadence=3600
+fi
+if [[ $output = '' ]]
+then
+	output=""
+fi
+if [[ $ignore_output = '' ]]
+then
+	ignore_output=""
+fi
+
+for item in command email_address description
+do
+	if [[ $item = '' ]]
+	then
+		echo $item needs to be set
+		exit 1
+	fi
+done
+
+echo python db.py --insert_alert '{"command":"'$command'","email_address":"'$email_address'","description":"'$description'","output":"'$output'","cadence":"'$cadence'","common_threshold":"'$common_threshold'","ignore_output":"'$ignore_output'"}'
+python db.py --insert_alert '{"command":"'$command'","email_address":"'$email_address'","description":"'$description'","output":"'$output'","cadence":"'$cadence'","common_threshold":"'$common_threshold'","ignore_output":"'$ignore_output'"}'
